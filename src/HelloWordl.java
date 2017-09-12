1afb0be5e9adcbcd26a9ac269c6a96ce0f4ad215
palesa <palesa@172.17.65.173>
2017-09-12 11:34:06 -0600

    added another print statement

--- src/HelloWordl.java
+++ src/HelloWordl.java
@@ -4,6 +4,7 @@ public class HelloWordl {
 	public HelloWordl() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Hello Worlda!");
+		System.out.println("What a challenge this was!");
 	}
 
 }
